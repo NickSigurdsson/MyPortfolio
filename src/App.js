d372b33141405dd2a35097510cfc5d7ba75d3efd
Nicholas Sigurdsson <nicholassigurdsson@gmail.com>
2023-01-29 16:52:33 +0700

    initial file cleaning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 
 function App() {
   return (
-    <div>Learn React</div>
+    <div>Testing</div>
   );
 }
 
+// import * as React from 'react';
+// import Button from '@mui/material/Button';
+
+// function App() {
+//   return <Button variant="contained">Hello World</Button>;
+// }
+
 export default App;
+
